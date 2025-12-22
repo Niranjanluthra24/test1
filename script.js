b596ec31d900cc7bc9c93669fc90725ddd7d5906
akshit696969 <akshitkohli9213@gmail.com>
2025-12-22 17:49:10 +0530

    remove

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const btn = document.getElementById("btn")
-const btn2 = document.getElementById("btn2")
+
 
 btn.addEventListener("click",()=>{
     alert("hello")
 })
-btn2.addEventListener("click",()=>{
-    let name = prompt("n: ")
-    console.log(name)
-})
+
