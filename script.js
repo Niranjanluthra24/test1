538ca3ea38ac418a1219d430e8a23375ea6af827
akshit696969 <akshitkohli9213@gmail.com>
2025-12-22 17:37:17 +0530

    js2

---
+++ script.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn")
+const btn2 = document.getElementById("btn2")
+
+btn.addEventListener("click",()=>{
+    alert("hello")
+})
+btn2.addEventListener("click",()=>{
+    let name = prompt("n: ")
+    console.log(name)
+})
